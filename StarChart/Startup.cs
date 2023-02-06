cb008d91840080871108485e1a33e1eeb482b79d
Rodney Canlas <47021958+rodneyjdc@users.noreply.github.com>
2023-02-06 14:06:50 -0800

    add and configure middleware in Startup.cs

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using StarChart.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace StarChart
 {
@@ -10,13 +12,19 @@ namespace StarChart
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            // Add support for MVC middleware
+            services.AddMvc();
+
+            // Point EntityFramework to application's DbContext
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("StarChart"));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            
+            app.UseMvc();
         }
     }
 }
