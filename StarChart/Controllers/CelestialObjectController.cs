c3548badb684395e3ab4235f7812cf96db19e2b8
Rodney Canlas <47021958+rodneyjdc@users.noreply.github.com>
2023-02-06 16:01:15 -0800

    created CelestialObjectController's Post, Put, Patch and Delete actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -96,7 +96,109 @@ namespace StarChart.Controllers
                 {
                     return NotFound();
                 }
-                
+
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CelestialObject obj)
+        {
+            try
+            {
+                _context.CelestialObjects.Add(obj);
+
+                if (_context.SaveChanges() > 0)
+                {
+                    return CreatedAtRoute("GetById", new { id = obj.Id }, obj);
+                }
+                else
+                {
+                    return BadRequest("New celestial object cannot be saved into the database.");
+                }
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, CelestialObject obj)
+        {
+            try
+            {
+                var existingObj = _context.CelestialObjects.Find(id);
+
+                if (existingObj != null)
+                {
+                    existingObj.Name = obj.Name;
+                    existingObj.OrbitalPeriod = obj.OrbitalPeriod;
+                    existingObj.OrbitedObjectId = obj.OrbitedObjectId;
+
+                    _context.CelestialObjects.Update(existingObj);
+                    _context.SaveChanges();
+
+                    return NoContent();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name)
+        {
+            try
+            {
+                var existingObj = _context.CelestialObjects.Find(id);
+
+                if (existingObj != null)
+                {
+                    existingObj.Name = name;
+                    _context.CelestialObjects.Update(existingObj);
+                    _context.SaveChanges();
+
+                    return NoContent();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var result = _context.CelestialObjects.Where(obj => obj.Id == id || obj.OrbitedObjectId == id).ToList();
+
+                if (result.Any())
+                {
+                    _context.CelestialObjects.RemoveRange(result);
+                    _context.SaveChanges();
+
+                    return NoContent();
+                }
+                else
+                {
+                    return NotFound();
+                }
             }
             catch (Exception)
             {
